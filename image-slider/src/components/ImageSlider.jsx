052d5eb0507823242aa7978fc04bc53ca2e9e3ad
Joyce Lau <jshizuki@yahoo.com.hk>
2024-07-08 18:46:25 +0900

    Show pictures without bypassing CORS for now

--- image-slider/src/components/ImageSlider.jsx
+++ image-slider/src/components/ImageSlider.jsx
@@ -1,9 +1,28 @@
-import { getImages } from '../utils/pexelsApi';
+import { useState, useEffect } from "react";
+import { getImages } from "../utils/pexelsApi";
 
 function ImageSlider() {
+  const [photoData, setPhotoData] = useState([]);
+
+  useEffect(() => {
+    getImages().then((images) => {
+      setPhotoData(images);
+    });
+  }, []);
+
+  console.log(photoData);
+
   return (
-    <>test</>
-  )
+    <div>
+      {photoData.map((data) => {
+        return (
+          <div key={data.id}>
+            <img src={data.url} alt={data.alt} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ImageSlider;
--- image-slider/src/utils/pexelsApi.js
+++ image-slider/src/utils/pexelsApi.js
@@ -1,8 +1,8 @@
 export const getImages = () => {
   const url =
-    "https://api.pexels.com/v1/search?query=Ocean&orientation=landscape&color=blue";
+    "https://api.pexels.com/v1/search?query=Ocean&orientation=landscape";
 
-  fetch(url, {
+  return fetch(url, {
     headers: {
       Authorization: process.env.PEXELS_API_KEY,
     },
@@ -12,13 +12,15 @@ export const getImages = () => {
         if (response.ok) {
           return response.json();
         }
-        throw new Error("Request failed!");
+        throw new Error(`Request failed with status ${response.status}!`);
       },
       (networkError) => console.log(networkError.message)
     )
     .then((jsonResponse) => {
       return jsonResponse.photos.map((photoData) => ({
-        url: photoData.url,
+        id: photoData.id,
+        url: photoData.src["medium"],
+        alt: photoData.alt,
       }));
     });
 };
